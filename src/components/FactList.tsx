19058b3708d59d7e97859c8771d6a1f5b6a141d8
alanfedrizzi <alanfedrizzi@gmail.com>
2025-03-17 10:12:40 +0100

    fact list sem facts

--- src/components/FactList.tsx
+++ src/components/FactList.tsx
@@ -8,13 +8,19 @@ interface FactListProps {
 function FactList({ facts }: FactListProps) {
   return (
     <section>
-      <ul className="facts-list">
-        {facts.map((fact) => (
-          <Fact fact={fact} key={fact.id} />
-        ))}
-      </ul>
+      {facts.length ? (
+        <>
+          <ul className="facts-list">
+            {facts.map((fact) => (
+              <Fact fact={fact} key={fact.id} />
+            ))}
+          </ul>
 
-      <p>There are {facts.length} facts in the database. Add your own!</p>
+          <p>There are {facts.length} facts in the database. Add your own!</p>
+        </>
+      ) : (
+        <p>There no facts registered in this category. Create the first one!</p>
+      )}
     </section>
   );
 }
